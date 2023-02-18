7c5f6072d47490e20303145fa9500a0aebc1ac1f
Hywenklis <hywenklis@hotmail.com>
2023-02-18 00:48:01 -0300

    Configura jackson, mongoDB e validation

---
+++ src/main/java/br/com/hr/reactiveflashcards/core/mongo/converter/DateToOffsetDateTimeConverter.java
@@ -0,0 +1,14 @@
+package br.com.hr.reactiveflashcards.core.mongo.converter;
+
+import org.springframework.core.convert.converter.Converter;
+
+import java.time.OffsetDateTime;
+import java.util.Date;
+
+public class DateToOffsetDateTimeConverter implements Converter<OffsetDateTime, Date> {
+
+    @Override
+    public Date convert(OffsetDateTime source) {
+        return Date.from(source.toInstant());
+    }
+}
