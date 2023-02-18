417d73dde73ca449fbda3e1daa9cada43565e501
Hywenklis <hywenklis@hotmail.com>
2023-02-18 15:18:23 -0300

    Tratamento de exception genericas na aplicação

--- src/main/java/br/com/hr/reactiveflashcards/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/br/com/hr/reactiveflashcards/api/exceptionhandler/ApiExceptionHandler.java
@@ -1,12 +1,25 @@
 package br.com.hr.reactiveflashcards.api.exceptionhandler;
 
+import br.com.hr.reactiveflashcards.api.controller.response.ProblemResponse;
+import br.com.hr.reactiveflashcards.domain.exception.ReactiveFlashcardsException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.annotation.Order;
+import org.springframework.core.io.buffer.DefaultDataBufferFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.MethodNotAllowedException;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebExceptionHandler;
 import reactor.core.publisher.Mono;
+
+import static br.com.hr.reactiveflashcards.domain.exception.BaseErrorMessage.GENERIC_EXCEPTION;
+import static br.com.hr.reactiveflashcards.domain.exception.BaseErrorMessage.GENERIC_METHOD_NOT_ALLOW;
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+import static org.springframework.http.HttpStatus.METHOD_NOT_ALLOWED;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
 
 @Component
 @Order(-2)
@@ -14,9 +27,69 @@ import reactor.core.publisher.Mono;
 @AllArgsConstructor
 public class ApiExceptionHandler implements WebExceptionHandler {
 
+    private final ObjectMapper objectMapper;
 
     @Override
     public Mono<Void> handle(ServerWebExchange exchange, Throwable ex) {
-        return null;
+        return Mono.error(ex)
+                .onErrorResume(Exception.class, e -> handleException(exchange, e))
+                .onErrorResume(ReactiveFlashcardsException.class, e -> handleReactiveFlashcardsException(exchange, e))
+                .onErrorResume(MethodNotAllowedException.class, e -> handleMethodNotAllowedException(exchange, e))
+                .onErrorResume(JsonProcessingException.class, e -> handleJsonProcessingException(exchange, e))
+                .then();
+    }
+
+    private Mono<Void> handleException(final ServerWebExchange exchange, final Exception ex) {
+        return Mono.fromCallable(() -> {
+                    prepareExchange(exchange, INTERNAL_SERVER_ERROR);
+                    return GENERIC_EXCEPTION.message();
+                }).map(message -> buildError(INTERNAL_SERVER_ERROR, message))
+                .doFirst(() -> log.error("=== Exception: ", ex))
+                .flatMap(problemResponse -> writeResponse(exchange, problemResponse));
+    }
+
+    private Mono<Void> handleReactiveFlashcardsException(final ServerWebExchange exchange, final ReactiveFlashcardsException ex) {
+        return Mono.fromCallable(() -> {
+                    prepareExchange(exchange, INTERNAL_SERVER_ERROR);
+                    return GENERIC_EXCEPTION.message();
+                }).map(message -> buildError(INTERNAL_SERVER_ERROR, message))
+                .doFirst(() -> log.error("=== ReactiveFlashcardsException: ", ex))
+                .flatMap(problemResponse -> writeResponse(exchange, problemResponse));
+    }
+
+    private Mono<Void> handleMethodNotAllowedException(final ServerWebExchange exchange, final MethodNotAllowedException ex) {
+        return Mono.fromCallable(() -> {
+                    prepareExchange(exchange, METHOD_NOT_ALLOWED);
+                    return GENERIC_METHOD_NOT_ALLOW.message();
+                }).map(message -> buildError(METHOD_NOT_ALLOWED, message))
+                .doFirst(() -> log.error("=== MethodNotAllowedException: Method [{}] is not allowed at [{}]",
+                        exchange.getRequest().getMethod(), exchange.getRequest().getPath().value(), ex))
+                .flatMap(problemResponse -> writeResponse(exchange, problemResponse));
+    }
+
+    private Mono<Void> handleJsonProcessingException(final ServerWebExchange exchange, final JsonProcessingException ex) {
+        return Mono.fromCallable(() -> {
+                    prepareExchange(exchange, METHOD_NOT_ALLOWED);
+                    return GENERIC_METHOD_NOT_ALLOW.message();
+                }).map(message -> buildError(METHOD_NOT_ALLOWED, message))
+                .doFirst(() -> log.error("=== JsonProcessingException: Failed to map exception for the request {} ",
+                        exchange.getRequest().getMethod(), ex))
+                .flatMap(problemResponse -> writeResponse(exchange, problemResponse));
+    }
+
+    private Mono<Void> writeResponse(ServerWebExchange exchange, ProblemResponse problemResponse) {
+        return exchange.getResponse()
+                .writeWith(Mono.fromCallable(() -> new DefaultDataBufferFactory()
+                        .wrap(objectMapper.writeValueAsBytes(problemResponse)))
+                );
+    }
+
+    private void prepareExchange(final ServerWebExchange exchange, final HttpStatus statusCode) {
+        exchange.getResponse().setStatusCode(statusCode);
+        exchange.getResponse().getHeaders().setContentType(APPLICATION_JSON);
+    }
+
+    private ProblemResponse buildError(final HttpStatus status, String errorDescription) {
+        return ProblemResponse.builder().status(status.value()).errorDescription(errorDescription).build();
     }
 }
--- src/main/java/br/com/hr/reactiveflashcards/domain/exception/BaseErrorMessage.java
+++ src/main/java/br/com/hr/reactiveflashcards/domain/exception/BaseErrorMessage.java
@@ -13,9 +13,9 @@ public class BaseErrorMessage {
     private final String key;
     private String[] params;
 
-    private static final BaseErrorMessage GENERIC_EXCEPTION = new BaseErrorMessage("generic");
-    private static final BaseErrorMessage GENERIC_NOT_FOUND = new BaseErrorMessage("generic.notFound");
-    private static final BaseErrorMessage GENERIC_METHOD_NOT_ALLOW = new BaseErrorMessage("generic.methodNotAllow");
+    public static final BaseErrorMessage GENERIC_EXCEPTION = new BaseErrorMessage("generic");
+    public static final BaseErrorMessage GENERIC_NOT_FOUND = new BaseErrorMessage("generic.notFound");
+    public static final BaseErrorMessage GENERIC_METHOD_NOT_ALLOW = new BaseErrorMessage("generic.methodNotAllow");
 
     public BaseErrorMessage params(final String... params) {
         this.params = ArrayUtils.clone(params);
