a5fd6547d28e59766e6ad19c555edeee68166b2b
Hywenklis <hywenklis@hotmail.com>
2023-02-17 23:51:14 -0300

    Configura docker e dependencias do projeto

---
+++ src/test/java/br/com/hr/reactiveflashcards/ReactiveFlashcardsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hr.reactiveflashcards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveFlashcardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
